059707b27be1c4f9e19ee7d428cb7bdd546ac1ac
Jettawat Vuttipakdee <130958616+JettawatV@users.noreply.github.com>
2025-02-08 16:48:48 -0500

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -222,10 +222,9 @@ st.subheader(f"Today's Insights")
 
 if binance_data:
     # Generate the insight text based on the selected crypto data
-    insight_text = f"""
-    {selected_crypto} is currently trading at ${ticker['price']:,.2f}. Over the last 24 hours, it has reached a high of ${float(binance_data['highPrice']):,.2f} and a low of ${float(binance_data['lowPrice']):,.2f}.
-    Trading volume for the past 24 hours was ${float(binance_data['quoteVolume']):,.2f} USD, with a market cap dominance of {float(dominance):,.2f}% of the total market. 
-    The 24H price change stands at {ticker['change']}%. """
+insight_text = f"""
+{selected_crypto} is currently trading at ${ticker['price']:,.2f}. Over the last 24 hours, it has reached a high of ${float(binance_data['highPrice']):,.2f} and a low of ${float(binance_data['lowPrice']):,.2f}. Trading volume for the past 24 hours was ${float(binance_data['quoteVolume']):,.2f} USD, with a market cap dominance of {float(dominance):,.2f}% of the total market. The 24H price change stands at {ticker['change']}%.
+"""
 
     # Display the generated insights text
     st.markdown(insight_text)
