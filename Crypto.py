21939d2a17db3dba81cc641c7e75cc5b945d93d7
Jettawat Vuttipakdee <130958616+JettawatV@users.noreply.github.com>
2025-02-08 15:21:54 -0500

    Update Crypto.py

--- Crypto.py
+++ Crypto.py
@@ -207,7 +207,7 @@ st.write(f"Last updated: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
 # Fetch Binance data
 binance_data = get_binance_data2(crypto_symbol)
-market_data = get_ath_and_marketcap_from_coinmarketcap(selected_crypto)
+market_data = get_ath_and_marketcap_from_coinmarketcap(crypto_symbol)
 dominance = get_crypto_dominance(crypto_symbol)
 open_interest = get_open_interest(crypto_symbol)
 
